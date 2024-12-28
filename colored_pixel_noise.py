f9ae37b07df074fa7ef9d86955f86084bca5a2af
Fe1kX <fe1kx@icloud.com>
2024-12-28 18:50:31 +0300

    Add files via upload

--- colored_pixel_noise.py
+++ colored_pixel_noise.py
@@ -21,11 +21,14 @@ def create_rgb_image(width, height, name, ext):
     image.save(f"{name}.{ext}")
 
 sg.theme('darkblue17')
-layout = [ [sg.Text('ДеГенератор картинок!', font=('YEARBOOK', 20))],
+menu_def = [['&File', ['!&Open', '&Save::savekey', '---', '&Properties', 'E&xit']], ['&Help', ['&About...']]]
+layout = [ [sg.Titlebar("ДеГенератор!")],
+            [sg.Text('ДеГенератор картинок!', font=('YEARBOOK', 20))],
             [sg.Push(), sg.Text("Ширина: "), sg.InputText("1000", size=(10)), sg.Push()],
             [sg.Push(), sg.Text("Высота: "), sg.InputText("1000", size=(10)), sg.Push()],
             [sg.Push(), sg.Text("Имя: "), sg.InputText("image", size=(10)), sg.Push()],
             [sg.Push(), sg.Text("Расширение: "), sg.Combo(['png', 'jpg', 'WebP', 'ico', 'bmp'], default_value='png'), sg.Push()],
+            #[sg.ProgressBar(10, key='-PROGRESS_BAR-', size=(27,2), bar_color='green')],
             [sg.Button('Отмена', button_color='#613434'), sg.Push(), sg.Button('ДеГенерировать', button_color='#128700')]]
             #[sg.Button('Отмена', button_color='#613434'), sg.Push(), sg.Button('Путь сохранения'), sg.Push(), sg.Button('ДеГенерировать', button_color='#128700')]]
 
